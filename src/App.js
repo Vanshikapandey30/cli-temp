e45a013d288c0301990d00dfa026ac60eb852279
Abhishek <zabhishekkale@gmail.com>
2021-04-12 23:43:01 +0530

    intermediate commit  - zzzz

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {React, useEffect, useState} from'react';
+// curl https://api.coinbase.com/v2/prices/BTC-USD/spot 
+const url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
+const options = {Authorization: 'Bearer 90df5f27a7b170cd775abf89d632b350b7c1c9d53e08b340cd9832ce52c2c'}
 function App() {
+  let dat = {};
+  let [ls ,setLs]  = useState([]);
+  useEffect(()=>{
+    fetch(url,options)
+  .then(res=>res.json())
+  .then(data=>
+    {dat = data.data.rates;
+      let im = []; 
+    for(let key  in dat){
+      let val = dat[key];
+      im.push({key,val});
+    }
+    setLs(im);
+    })
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h4>cli-temp</h4>
+        <h4>cli-crypto</h4>
       </header>
-      <div class="container">
-        from container
+      <div className="container">
+        Tracking Bitcoins
+        <ul>
+          {ls.map((val,idx)=><li key={idx}>{val.key} {val.val}</li>) }
+        </ul>
       </div>
     </div>
   );
